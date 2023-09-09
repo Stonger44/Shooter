029c6d7868663aea8f875283f73c40fab95e6999
Brian Stong <49844364+Stonger44@users.noreply.github.com>
2023-09-08 21:32:33 -0700

    Player script with basic movement.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private Vector3 _direction;
+    [SerializeField] private float _horizontalAxis;
+    [SerializeField] private float _verticalAxis;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        _horizontalAxis = Input.GetAxis("Horizontal");
+        _verticalAxis = Input.GetAxis("Vertical");
+
+        _direction.x = _horizontalAxis;
+        _direction.y = _verticalAxis;
+
+        this.transform.Translate(_direction * _speed * Time.deltaTime);
+    }
+}
+
+
+
+
