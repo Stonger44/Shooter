69cca61701fb7674d7d96a7a3f89b44649d3dc41
Brian Stong <49844364+Stonger44@users.noreply.github.com>
2023-10-15 19:01:59 -0700

    Changed variable and method names from 'flicker' to 'blink'

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -9,7 +9,7 @@ public class UIManager : MonoBehaviour
     [SerializeField] private Image _lives;
     [SerializeField] private Sprite[] _livesSpriteArray;
     [SerializeField] private GameObject _gameOver;
-    [SerializeField] private float _gameOverFlickerTime = 0.5f;
+    [SerializeField] private float _gameOverBlinkTime = 0.5f;
     private bool _gameOverActive = false;
 
     // Start is called before the first frame update
@@ -39,18 +39,18 @@ public class UIManager : MonoBehaviour
             Time.timeScale = 1f;
             Time.fixedDeltaTime = 0.02f * Time.timeScale;
 
-            StartCoroutine(GameOverFlicker(lives));
+            StartCoroutine(GameOverBlink(lives));
         }
     }
 
-    private IEnumerator GameOverFlicker(int lives)
+    private IEnumerator GameOverBlink(int lives)
     {
         while (lives < 1)
         {
             _gameOverActive = !_gameOverActive;
             _gameOver.SetActive(_gameOverActive);
 
-            yield return new WaitForSeconds(_gameOverFlickerTime);
+            yield return new WaitForSeconds(_gameOverBlinkTime);
         }
     }
 }
