623e2c5bf560404a55e0c5e675235797fc93080c
Brian Stong <49844364+Stonger44@users.noreply.github.com>
2023-10-15 17:43:15 -0700

    Created GAME OVER text and flicker behavior

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,6 +8,9 @@ public class UIManager : MonoBehaviour
     [SerializeField] private Text _score;
     [SerializeField] private Image _lives;
     [SerializeField] private Sprite[] _livesSpriteArray;
+    [SerializeField] private GameObject _gameOver;
+    [SerializeField] private float _gameOverFlickerTime = 0.5f;
+    private bool _gameOverActive = false;
 
     // Start is called before the first frame update
     void Start()
@@ -29,5 +32,25 @@ public class UIManager : MonoBehaviour
     public void UpdateLives(int lives)
     {
         _lives.sprite = _livesSpriteArray[lives];
+
+        if (lives < 1)
+        {
+            // Set game time back to normal in case SpeedBoost is active when the player dies
+            Time.timeScale = 1f;
+            Time.fixedDeltaTime = 0.02f * Time.timeScale;
+
+            StartCoroutine(GameOverFlicker(lives));
+        }
+    }
+
+    private IEnumerator GameOverFlicker(int lives)
+    {
+        while (lives < 1)
+        {
+            _gameOverActive = !_gameOverActive;
+            _gameOver.SetActive(_gameOverActive);
+
+            yield return new WaitForSeconds(_gameOverFlickerTime);
+        }
     }
 }
