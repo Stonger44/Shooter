728b5afd79cfb0f9472a9e4390141c2812a6226c
Brian Stong <49844364+Stonger44@users.noreply.github.com>
2023-10-31 13:35:30 -0700

    Created Quit button on Main Menu

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -21,4 +21,9 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(1); // Game Scene
     }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
 }
