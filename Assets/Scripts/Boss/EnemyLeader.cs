ba85c2721b5c4d5a7b458b32565b336ec199fe2e
Brian Stong <49844364+Stonger44@users.noreply.github.com>
2023-12-28 20:43:21 -0800

    Setting up EnemyLeader approach and holding movement

--- Assets/Scripts/Boss/EnemyLeader.cs
+++ Assets/Scripts/Boss/EnemyLeader.cs
@@ -6,15 +6,17 @@ using UnityEngine;
 public class EnemyLeader : SpaceShip
 {
     [Header("Boundaries")]
-    [SerializeField] private float _leftBoundary = 11f;
-    [SerializeField] private float _rightBoundary = 21f;
+    [SerializeField] private float _leftBoundary = 10.75f;
+    [SerializeField] private float _rightBoundary = 11f;
     [SerializeField] private float _upperBoundary = 0.3f;
-    [SerializeField] private float _lowerBoundary = -0.3f;
+    [SerializeField] private float _lowerBoundary = -1f;
 
     [Header("Movement")]
     [SerializeField] private float _speed = 2f;
     private Vector2 _direction = Vector2.left;
     private bool _holdPosition = false;
+    [SerializeField] private float _xDirection = -1f;
+    [SerializeField] private float _yDirection = 0.2f;
 
     [Header("PowerCore")]
     [SerializeField] private GameObject _powerCore;
@@ -33,30 +35,48 @@ public class EnemyLeader : SpaceShip
     // Update is called once per frame
     void Update()
     {
-        if (!_holdPosition)
-        {
-            Approach();
-        }
-        else
-        {
-            Move();
-        }
-        
-    }
-
-    private void Approach()
-    {
-        transform.Translate(_direction * _speed * Time.deltaTime);
-
-        if (transform.position.x <= 11f)
-        {
-            _holdPosition = true;
-        }
+        Move();
     }
 
     private void Move()
     {
-        
+        if (transform.position.y >= _upperBoundary || transform.position.y <= _lowerBoundary)
+        {
+            Change_Y_Direction();
+        }
+
+        transform.Translate(_direction * _speed * Time.deltaTime);
+
+        if (_holdPosition)
+        {
+            transform.position = new Vector2(Mathf.Clamp(transform.position.x, _leftBoundary, _rightBoundary), Mathf.Clamp(transform.position.y, _lowerBoundary, _upperBoundary));
+        }
+        else
+        {
+            CheckApproach();
+        }
     }
 
+    private void CheckApproach()
+    {
+        if (transform.position.x <= _rightBoundary)
+        {
+            float yDirection = UnityEngine.Random.value < 0.5f ? -_yDirection : _yDirection;
+            _direction = new Vector2(_xDirection, yDirection);
+
+            _holdPosition = true;
+        }
+    }
+
+    private void Change_Y_Direction()
+    {
+        if (transform.position.y >= _upperBoundary)
+        {
+            _direction.y = -_yDirection;
+        }
+        else if (transform.position.y <= _lowerBoundary)
+        {
+            _direction.y = _yDirection;
+        }
+    }
 }
